faa39cd1b89ee6edf9c62e2ce47e45df950aac45
AnshhSingh <anshsingh25bd@gmail.com>
2023-10-18 19:47:47 +0530

    hosting

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ const Header = () => {
                         <li>Categories</li>
                     </ul>
                     <div className="center" onClick={() => navigate("/")}>
-                        e-commerce
+                        E-commerce
                     </div>
                     <div className="right">
                         <TbSearch onClick={() => setSearchModal(true)} />
